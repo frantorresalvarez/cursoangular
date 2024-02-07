5e780de7b3e36ef28f5cfa6ef0592e9143ef8aef
= <=>
2024-02-07 22:35:09 +0100

    3

--- segundoproyecto/src/app/app.module.ts
+++ segundoproyecto/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { CabaceraComponent } from './cabacera/cabacera.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
+import { MenuComponent } from './menu/menu.component';
+import { PancartaComponent } from './pancarta/pancarta.component';
+import { DestacadosComponent } from './destacados/destacados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabaceraComponent,
     PrincipalComponent,
-    PiedepaginaComponent
+    PiedepaginaComponent,
+    MenuComponent,
+    PancartaComponent,
+    DestacadosComponent
   ],
   imports: [
     BrowserModule,
---
+++ segundoproyecto/src/app/destacados/destacados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destacados',
+  templateUrl: './destacados.component.html',
+  styleUrl: './destacados.component.css'
+})
+export class DestacadosComponent {
+    numero_articulos: number = 10;
+    articulos = Array(this.numero_articulos).fill(0)
+    
+}
